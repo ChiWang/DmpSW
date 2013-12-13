4f0986cc4ed8eb7f040eba4024221f5e3db8fb4b
ChiWang <chiwang@mail.ustc.edu.cn>
2013-12-13 10:43:54 +0800

    modify DmpRdcMain.cc

--- Kernel/Readme.md
+++ Kernel/Readme.md
@@ -1,3 +1,15 @@
 
 
-All abstract classes in here.
+DAMPE Software kernel:
+
+    1.  commom abstract classes, not one package will use those classes. (If only one package will use a abstract class, we put it in the certain package).
+
+    2.  this package not depend on other package, so, if like DmpEvtHeader(it is not a abstruct class,) we must put it in here.
+
+Usage:
+
+    1.  execute "./createDictionary" to create Kernel_Dict.h and Kernel_Dict.cc
+
+    2.  execute "scons" to create shared library.
+
+
--- RawDataConversion/DmpRdc/src/DmpRdcMain.cc
+++ RawDataConversion/DmpRdc/src/DmpRdcMain.cc
@@ -32,48 +32,47 @@ int main(int argc, char* argv[]){
   if ( ! RdcMan->SetConnectorBgo() ) return 1;
   if ( ! RdcMan->SetConnectorNud() ) return 1;
 
-  TString inFileName = "DmpRdcInput.infor";
-  string note;
+  TString inFileName;
   if (argc > 2) {
     Usage();
     return 1;
   } else if (argc == 2) {
-    inFileName=argv[1];
-    if (inFileName.Contains(".dat")) {
-      note = argv[1];
-      unsigned found = note.find_last_of("/\\");
-      string path = note.substr(0,found);
-      string name = note.substr(found+1);
-//      RdcMan->SetInDataPath(XXX);
-//      RdcMan->SetDataName(XXX);
-      RdcMan->Core();
-    }
+    inFileName = argv[1];
   } else {
-    if ( inFileName.Contains(".infor") ) {
-      ifstream* BatchInfor = new ifstream (inFileName,ios::in);
-      if (!BatchInfor->good()) {
-        cout<<"\n\t---->Error: there's not a file named "<<inFileName<<endl;
-        return 1;
-      }
-      getline(*BatchInfor,note);    // reserved 1 lines for note
-      getline(*BatchInfor,note);
-      RdcMan->SetInDataPath(note);
-      getline(*BatchInfor,note);    // reserved 1 lines for note
-      getline(*BatchInfor,note);
-      RdcMan->SetOutDataPath(note);
-      getline(*BatchInfor,note);    // reserved 1 lines for note
-      while (note != "END") {
-        *BatchInfor>>note;
-        if (note == "END") break;
-        RdcMan->SetDataName((TString)note);
-        RdcMan->Core();
-      }
-      BatchInfor->close();
-      delete BatchInfor;
-    } else {
-      Usage();
+    inFileName = "DmpRdcInput.infor";
+  }
+
+  string note;
+  if (inFileName.Contains(".dat")) {
+    note = inFileName;
+    unsigned found = note.find_last_of("/\\");
+    RdcMan->SetInDataPath((TString)note.substr(0,found));
+    RdcMan->SetDataName((TString)note.substr(found+1));
+    RdcMan->Core();
+  } else if (inFileName.Contains(".infor")) {
+    ifstream* BatchInfor = new ifstream (inFileName,ios::in);
+    if (!BatchInfor->good()) {
+      cerr<<"\n\tRDC batch mode: there's not a file named "<<inFileName<<endl;
       return 1;
     }
+    getline(*BatchInfor,note);    // reserved 1 lines for note
+    getline(*BatchInfor,note);
+    RdcMan->SetInDataPath(note);
+    getline(*BatchInfor,note);    // reserved 1 lines for note
+    getline(*BatchInfor,note);
+    RdcMan->SetOutDataPath(note);
+    getline(*BatchInfor,note);    // reserved 1 lines for note
+    while (note != "END") {
+      *BatchInfor>>note;
+      if (note == "END") break;
+      RdcMan->SetDataName((TString)note);
+      RdcMan->Core();
+    }
+    BatchInfor->close();
+    delete BatchInfor;
+  } else {
+    Usage();
+    return 1;
   }
 
   DmpRdc::Clear();
@@ -83,5 +82,5 @@ int main(int argc, char* argv[]){
 
 //------------------------------------------------------------------------------
 void Usage(){
-  cout<<"Usage:\tXXX"<<endl;
+  cerr<<"Usage:\tXXX"<<endl;
 }
