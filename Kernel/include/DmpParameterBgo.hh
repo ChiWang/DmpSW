6947cb3dceb67f286336850dca53500a300326eb
Chi Wang <chiwang@mail.ustc.edu.cn>
2013-12-14 10:26:11 +0800

    delete DetectorCondition. mv connector into RDC mv Parameter into Kernel

---
+++ Kernel/include/DmpParameterBgo.hh
@@ -0,0 +1,28 @@
+/*=====================================================================
+ *   File:   DmpDcdParameterBgo.hh
+ *   Author: Chi WANG  (chiwang@mail.ustc.edu.cn)    14/12/2013
+ *---------------------------------------------------------------------
+ *   Description:
+ *
+ *---------------------------------------------------------------------
+ *   History:
+ *                           Last update:  14/12/2013   10:20:08
+=====================================================================*/
+
+#ifndef DmpDcdParameterBgo_H
+#define DmpDcdParameterBgo_H
+
+namespace DmpDcdParameter{
+
+  namespace Bgo{            //Sub-detector  Bgo parameters                             
+    const short kPlaneNb = 1;
+    const short kBarNb   = 22;
+    const short kRefBarNb= 2;
+    const short kSideNb  = 2;
+    const short kDyNb    = 3;
+  }
+
+}
+
+#endif
+
---
+++ Kernel/include/DmpParameterNud.hh
@@ -0,0 +1,19 @@
+
+#ifndef DmpDcdParameterNud_H
+#define DmpDcdParameterNud_H
+
+namespace DmpDcdParameter{
+
+  namespace Nud{            //Sub-detector  Nud parameters                             
+    const short kPlaneNb = 6;
+    const short kBarNb   = 11;
+    const short kRefBarNb= 2;
+    const short kSideNb  = 1;
+    const short kDyNb    = 3;
+    const short kStdDataLength = ((kBarNb+kRefBarNb)*kDyNb*2+3)*2;          //3: (1)data Length 0x00a2; (2)reverse 0x0000; (3)CRC 0x0xxx.  *2:to unit Byte
+  }
+
+}
+
+#endif
+
