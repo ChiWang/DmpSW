f62e99d4d88c41a39d658d7f7f84105217f1d67f
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-01-22 12:26:31 +0800

    new file:   Event/Bgo/include/DmpRecordBgoPed.hh new file:   Event/Bgo/src/DmpRecordBgoPed.cc new file:   Event/DmpEvent/src/DmpVRecord.cc new file:   Event/Stk/include/DmpEventStkLinkDef.hh

---
+++ Event/Bgo/include/DmpRecordBgoPed.hh
@@ -0,0 +1,38 @@
+/*=====================================================================
+ *   File:   DmpRecordBgoPed.hh
+ *   Author: Chi WANG  (chiwang@mail.ustc.edu.cn)    26/12/2013
+ *---------------------------------------------------------------------
+ *   Description:
+ *
+ *---------------------------------------------------------------------
+ *   History:
+ *                           Last update:  26/12/2013   21:22:38
+=====================================================================*/
+
+#ifndef DmpRecordBgoPed_H
+#define DmpRecordBgoPed_H
+
+#include "DmpVRecord.hh"
+
+class DmpRecordBgoPed : public DmpVRecord {
+/*
+ * DmpRecordBgoPed
+ *
+ *  used in dmpBgoPed
+ */
+
+ public:
+  DmpRecordBgoPed();
+  ~DmpRecordBgoPed();
+
+ private:
+  std::vector<int>      fLBSDCollection;
+  std::vector<double>   fMeanCollection;
+  std::vector<double>   fSigmaCollection;
+
+
+  ClassDef(DmpRecordBgoPed,1)
+};
+
+#endif
+
---
+++ Event/Stk/include/DmpEventStkLinkDef.hh
@@ -0,0 +1,25 @@
+/*=====================================================================
+ *   File:   DmpEvent_Stk_LinkDef.hh
+ *   Author: Chi WANG  (chiwang@mail.ustc.edu.cn)    13/12/2013
+ *---------------------------------------------------------------------
+ *   Description:
+ *
+ *---------------------------------------------------------------------
+ *   History:
+ *                           Last update:  22/01/2014   11:31:13
+=====================================================================*/
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class DmpEvtStkRaw+;
+//#pragma link C++ class DmpEvtStkHit+;
+//#pragma link C++ class DmpEvtStkCluster+;
+//#pragma link C++ class DmpEvtStkShower+;
+//#pragma link C++ class DmpRecordStkPed+;
+
+#endif
+
