5819514d94e2f8c1dd385ee18f4ab723513a1f6c
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-03-17 21:41:44 +0800

    modified:   Geometry/Geometry.scons modified:   Simulation/DmpSim/src/DmpSimPrimaryGeneratorAction.cc modified:   Simulation/share/DmpSim_electron.mac modified:   Simulation/share/DmpSim_proton.mac modified:   Simulation/share/JobOpt_DmpSim

--- Simulation/DmpSim/src/DmpSimPrimaryGeneratorAction.cc
+++ Simulation/DmpSim/src/DmpSimPrimaryGeneratorAction.cc
@@ -1,5 +1,5 @@
 /*
- *  $Id: DmpSimPrimaryGeneratorAction.cc, 2014-03-03 23:21:07 chi $
+ *  $Id: DmpSimPrimaryGeneratorAction.cc, 2014-03-17 20:37:33 chi $
  *  Author(s):
  *    Chi WANG (chiwang@mail.ustc.edu.cn) 03/03/2014
 */
@@ -13,8 +13,8 @@ DmpSimPrimaryGeneratorAction::DmpSimPrimaryGeneratorAction()
  :fGPS(0)
 {
   fGPS = new G4GeneralParticleSource();
-  fGPS->SetNumberOfParticles(1);
-//  fGPS->SetParticleDefinition(particle);
+  //fGPS->SetNumberOfParticles(1);
+  //fGPS->SetParticleDefinition(particle);
 }
 
 DmpSimPrimaryGeneratorAction::~DmpSimPrimaryGeneratorAction(){
@@ -22,7 +22,7 @@ DmpSimPrimaryGeneratorAction::~DmpSimPrimaryGeneratorAction(){
 }
 
 void DmpSimPrimaryGeneratorAction::GeneratePrimaries(G4Event *anEvent){
-std::cout<<"DEBUG: "<<__FILE__<<"("<<__LINE__<<"), in "<<__PRETTY_FUNCTION__<<std::endl;
+//std::cout<<"DEBUG: "<<__FILE__<<"("<<__LINE__<<"), in "<<__PRETTY_FUNCTION__<<std::endl;
   fGPS->GeneratePrimaryVertex(anEvent);
   DmpSimDataManager::GetInstance()->UpdatePrimaryParticleInformation(anEvent);
 }
