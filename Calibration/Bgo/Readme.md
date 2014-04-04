af07478e4b50320632e4352af63a331270f4ee51
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-04-04 17:10:28 +0800

    update

--- Calibration/Bgo/Readme.md
+++ Calibration/Bgo/Readme.md
@@ -1,11 +1,3 @@
 
 ##   Special Calibration for Bgo
 
-###  Structure
-
-    *   Mip
-    create one commond "dmpCalL0SBgoMip" for Bgo to find Mip
-
-    *   Relation
-    create one commond "dmpCalL0SBgoRel" for Bgo to find Relation
-    
---
+++ Event/Bgo/src/DmpCalBgo.cc
@@ -0,0 +1,22 @@
+/*
+ *  $Id: DmpCalBgo.cc, 2014-04-04 13:55:38 chi $
+ *  Author(s):
+ *    Chi WANG (chiwang@mail.ustc.edu.cn) 04/04/2014
+*/
+
+#include "DmpCalBgo.h"
+
+DmpCalBgo::DmpCalBgo()
+ :fUsedDate_Ped("no"),
+  fUsedDate_Mip("no"),
+  fUsedDate_Rel("no")
+{
+  fPgkID += "CalBgo_V1.0_";
+  //fUsedDate_Ped 
+}
+
+
+//#include <boost/filesystem/path.hpp>
+  //boost::filesystem::path inpath(GetInData());
+
+
