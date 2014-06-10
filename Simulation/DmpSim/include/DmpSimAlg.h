7d559618848a09b8b36976b6bfbf84651cfe71ff
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-06-10 18:38:43 +0800

    Simu DmpSimAlg

---
+++ Simulation/DmpSim/include/DmpSimAlg.h
@@ -0,0 +1,40 @@
+/*
+ *  $Id: DmpSimAlg.h, 2014-06-10 14:57:40 DAMPE $
+ *  Author(s):
+ *    Chi WANG (chiwang@mail.ustc.edu.cn) 10/06/2014
+*/
+
+#ifndef DmpSimAlg_H
+#define DmpSimAlg_H
+
+#include "DmpVAlg.h"
+
+class DmpSimRunManager;
+class G4PhysListFactory;
+
+class DmpSimAlg : public DmpVAlg{
+/*
+ *  DmpSimAlg
+ *
+ *
+ */
+public:
+  DmpSimAlg();
+  ~DmpSimAlg();
+
+  void Set(const std::string &type,const std::string &argv);
+  bool Initialize();
+  bool ProcessThisEvent();
+  bool Finalize();
+
+private:
+  DmpSimRunManager      *fSimRunMgr;    // run manager of simulation
+  G4PhysListFactory     *fPhyFactory;
+  std::string           fMacFile;
+  std::string           fPhyListName;   // default is QGSP_BIC
+  long                  fEventID;
+  long                  fSeed;          // repeat one job, by setting the same seed
+
+};
+
+#endif
---
+++ Simulation/DmpSim/src/DmpSimAlg.cc
@@ -0,0 +1,158 @@
+/*
+ *  $Id: DmpSimAlg.cc, 2014-06-10 16:04:34 DAMPE $
+ *  Author(s):
+ *    Chi WANG (chiwang@mail.ustc.edu.cn) 10/06/2014
+*/
+
+#include "DmpSimAlg.h"
+#include "DmpSimRunManager.h"
+#include "G4PhysListFactory.hh"
+#include "DmpSimDetector.h"
+#include "DmpSimPrimaryGeneratorAction.h"
+#include "DmpSimTrackingAction.h"
+#include "DmpCore.h"
+#include "G4UImanager.hh"
+#ifdef G4UI_USE
+#include "G4UIExecutive.hh"
+#endif
+#ifdef G4VIS_USE
+#include "G4VisExecutive.hh"
+#endif
+
+//-------------------------------------------------------------------
+DmpSimAlg::DmpSimAlg()
+ :DmpVAlg("Sim/Alg/RunManager"),
+  fSimRunMgr(0),
+  fPhyFactory(0),
+  fMacFile("VIS"),
+  fPhyListName("QGSP_BIC"),
+  fEventID(0),
+  fSeed(0)
+{
+  fPhyFactory = new G4PhysListFactory();
+  OptMap.insert(std::make_pair("Physics",0));
+  OptMap.insert(std::make_pair("Gdml",1));
+  OptMap.insert(std::make_pair("Nud/DeltaTime",2));
+  OptMap.insert(std::make_pair("MacFile",3));
+  OptMap.insert(std::make_pair("Seed",4));
+}
+
+//-------------------------------------------------------------------
+DmpSimAlg::~DmpSimAlg(){
+  delete fSimRunMgr;
+  delete fPhyFactory;
+}
+
+//-------------------------------------------------------------------
+#include <boost/lexical_cast.hpp>
+#include "DmpEvtMCNudMSD.h"
+void DmpSimAlg::Set(const std::string &type,const std::string &argv){
+  if(OptMap.find(type) == OptMap.end()){
+    DmpLogError<<" No argument type "<<type<<DmpLogEndl;
+  }
+  switch (OptMap[type]){
+    case 0:
+    {// Physics
+      fPhyListName = argv;
+      break;
+    }
+    case 1:
+    {// Gdml
+      DmpSimDetector::SetGdml(argv);
+      break;
+    }
+    case 2:
+    {// Nud/DeltaTime
+      DmpEvtMCNudMSD::SetDeltaTime(boost::lexical_cast<short>(argv));
+      break;
+    }
+    case 3:
+    {// MacFile
+      fMacFile = argv;
+      break;
+    }
+    case 4:
+    {// Seed
+      fSeed = boost::lexical_cast<long>(argv);
+      std::cout<<" [seed] "<<fSeed<<DmpLogEndl;
+      break;
+    }
+  }
+}
+
+//-------------------------------------------------------------------
+#include <time.h>
+#include "CLHEP/Random/Random.h"
+#include <stdlib.h>     // getenv()
+bool DmpSimAlg::Initialize(){
+// set G4 kernel
+  fSimRunMgr = new DmpSimRunManager();
+  fSimRunMgr->SetUserInitialization(fPhyFactory->GetReferencePhysList(fPhyListName));
+  fSimRunMgr->SetUserAction(new DmpSimPrimaryGeneratorAction());      // only Primary Generator is mandatory
+  fSimRunMgr->SetUserInitialization(new DmpSimDetector());
+  fSimRunMgr->SetUserAction(new DmpSimTrackingAction());
+  fSimRunMgr->Initialize();
+// set random seed
+  if(0 == fSeed){
+    long seed = time((time_t*)NULL);
+    CLHEP::HepRandom::setTheSeed(seed);
+    std::cout<<" [seed] "<<seed<<DmpLogEndl;
+    //CLHEP::HepRandom::showEngineStatus(); 
+  }else{
+    CLHEP::HepRandom::setTheSeed(fSeed);
+  }
+// boot simulation
+  if("VIS" == fMacFile){    // vis mode
+    G4UImanager *uiMgr = G4UImanager::GetUIpointer();
+#ifdef G4UI_USE
+    char *dummyargv[20]={"visual"};
+    G4UIExecutive *ui = new G4UIExecutive(1,dummyargv);
+#ifdef G4VIS_USE
+    G4VisExecutive *vis = new G4VisExecutive();
+    vis->Initialize();
+    G4String prefix = (G4String)getenv("DMPSWSYS")+"/share/Simulation/";
+    uiMgr->ApplyCommand("/control/execute "+prefix+"DmpSimVis.mac");
+#endif
+    if (ui->IsGUI()){
+      uiMgr->ApplyCommand("/control/execute "+prefix+"DmpSimGUI.mac");
+    }
+    ui->SessionStart();
+    delete ui;
+#endif
+  }else{    // batch mode
+    // if not vis mode, do some prepare for this run. refer to G4RunManagr::BeamOn()
+    if(fSimRunMgr->ConfirmBeamOnCondition()){
+      fSimRunMgr->ConstructScoringWorlds();
+      fSimRunMgr->RunInitialization();
+      // *
+      // *  TODO:  check G4RunManager::InitializeEventLoop(the third argument right?)
+      // *
+      fSimRunMgr->InitializeEventLoop(gCore->GetMaxEventNumber(),fMacFile.c_str(),gCore->GetMaxEventNumber());
+    }else{
+      DmpLogError<<"G4RunManager::Initialize() failed"<<DmpLogEndl;
+      return false;
+    }
+  }
+  return true;
+}
+
+//-------------------------------------------------------------------
+bool DmpSimAlg::ProcessThisEvent(){
+  if("VIS" == fMacFile){
+    return true;
+  }
+  if(fSimRunMgr->SimOneEvent(fEventID)){
+    ++fEventID;
+    return true;
+  }
+  return false;
+}
+
+//-------------------------------------------------------------------
+bool DmpSimAlg::Finalize(){
+  if("VIS" != fMacFile){
+    fSimRunMgr->TerminateEventLoop();
+    fSimRunMgr->RunTermination();
+  }
+  return true;
+}
